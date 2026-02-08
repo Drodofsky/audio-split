3beee98d839b589823ebcaec2a868dfd5b0f2b03
Felix Drodofsky <drodofsky@protonmail.com>
2026-02-08 12:55:13 +0100

    impl view

--- src/audio_split/mod.rs
+++ src/audio_split/mod.rs
@@ -1,4 +1,4 @@
-use iced::{Element, widget};
+use iced::{Element, Length, widget};
 
 pub struct AudioSplit {}
 
@@ -8,8 +8,17 @@ impl AudioSplit {
     }
     pub fn update(&mut self, _message: Message) {}
     pub fn view(&self) -> Element<'_, Message> {
-        widget::text("Hello World").into()
+        widget::column![self.view_top(), self.view_center()].into()
+    }
+    fn view_top(&self) -> Element<'_, Message> {
+        widget::row![widget::button("open audio file")].into()
+    }
+    fn view_center(&self) -> Element<'_, Message> {
+        widget::container(widget::text("Hello World"))
+            .center(Length::Fill)
+            .into()
     }
 }
 
+#[derive(Debug, Clone)]
 pub enum Message {}
